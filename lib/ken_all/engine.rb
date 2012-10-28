45f5be1ecb2d0a8d0c7ac708e3d3918d6687cf35
Yamamoto Kazuhisa <ak.hisashi@gmail.com>
2012-10-28 18:24:49 +0900

    use rspec
    see.
    http://whilefalse.net/2012/01/25/testing-rails-engines-rspec/#

--- lib/ken_all/engine.rb
+++ lib/ken_all/engine.rb
@@ -1,5 +1,9 @@
 module KenAll
   class Engine < ::Rails::Engine
+    config.generators do |g|
+      g.test_framework :rspec
+      g.integration_tool :rspec
+    end
     isolate_namespace KenAll
   end
 end
