30ef1d0b753d2cbc15813ae76f080b20fb6eb372
Yamamoto Kazuhisa <ak.hisashi@gmail.com>
2013-07-22 19:44:59 +0900

    DB項目の型を変更

---
+++ db/migrate/20130722102956_column_change_postal_codes.rb
@@ -0,0 +1,10 @@
+class ColumnChangePostalCodes < ActiveRecord::Migration
+  def change
+    change_column :ken_all_postal_codes, :address1, :text
+    change_column :ken_all_postal_codes, :address2, :text
+    change_column :ken_all_postal_codes, :address3, :text
+    change_column :ken_all_postal_codes, :address_kana1, :text
+    change_column :ken_all_postal_codes, :address_kana2, :text
+    change_column :ken_all_postal_codes, :address_kana3, :text
+  end
+end
